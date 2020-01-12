94154f003944c2b1080ccc45118b716765732d2f
Robo <59576654+kulr00@users.noreply.github.com>
2020-01-12 14:18:09 +0100

    User TC´s complete

--- src/test/java/cz/vse/selenium/TestLogin.java
+++ src/test/java/cz/vse/selenium/TestLogin.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestLogin {
 
@@ -86,7 +88,39 @@ public class TestLogin {
         Assert.assertTrue(logo!=null);
 
     }
+    @Test
+    public void logOff(){
 
+        //GIVEN
+
+        driver.get(PREFIX);
+        //Login
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("rukovoditel");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("vse456ru");
+        WebElement button =driver.findElement(By.cssSelector(".btn"));
+        button.click();
+        Assert.assertTrue(driver.getTitle().startsWith("Rukovoditel | Dashboard"));
+
+        //WHEN
+        WebElement menu = driver.findElement(By.cssSelector(".fa-angle-down"));
+        menu.click();
+
+        //Čakanie nebolo potrebné
+        /*WebDriverWait wait = new WebDriverWait(driver, 1);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Logoff")));*/
+        WebElement logOffButton = driver.findElement(By.linkText("Logoff"));
+        logOffButton.click();
+
+        //THEN
+        Assert.assertTrue(driver.getTitle().startsWith("Rukovoditel"));
+        WebElement h3 = driver.findElement(By.className("form-title"));
+        Assert.assertTrue(h3.getText().equals("Login"));
+
+
+    }
 
 }
 
